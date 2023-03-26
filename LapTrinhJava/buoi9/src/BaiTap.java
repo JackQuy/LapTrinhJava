3161132cef6ad743a190913fa2185a19548ec770
JackQuy <phquy9192@gmail.com>
2023-03-26 21:08:12 +0700

    buoi 11

--- LapTrinhJava/buoi9/src/BaiTap.java
+++ LapTrinhJava/buoi9/src/BaiTap.java
@@ -1,2 +1,92 @@
-package PACKAGE_NAME;public class BaiTap {
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BaiTap {
+
+    public static void main(String[] args) {
+        int n;
+        n = nhapSoNguyen();
+
+        int a[] = new int[n];
+
+        nhapMang(a, n);
+        xuatMang(a, n);
+
+        if (checkMangToanChan(a, n)) {
+            System.out.println("Mảng a toàn số chẵn");
+        } else {
+            System.out.println("Mảng a không phải toàn số chẵn");
+        }
+
+        System.out.println("Tổng số các nguyên tố của mảng: " + totalprimeNumber(a, n));
+
+    }
+    static int nhapSoNguyen() {
+        int n;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Nhập vào n: ");
+            n = scanner.nextInt();
+            if (n < 0) {
+                System.out.println("ban da nhap sai, vui long nhap lai!");
+            }
+        } while (n < 0);
+
+        return n;
+    }
+
+    static void nhapMang(int arr[], int n) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap mang arr có do dai " + n + " :");
+        for (int i = 0; i < n; i++) {
+            System.out.print("a[" + i + "] = ");
+            arr[i] = scanner.nextInt();
+        }
+    }
+
+    static void xuatMang(int arr[], int n) {
+        System.out.println("Xuat mang arr có do dai " + n + " :");
+        for (int i = 0; i < n; i++) {
+            System.out.println("a[" + i + "] = " + arr[i]);
+        }
+    }
+
+    static boolean checkSoChan(int n) {
+        return n % 2 == 0;
+    }
+
+    static boolean checkMangToanChan(int arr[], int n) {
+        for (int i = 0; i < n; i++) {
+            if (!checkSoChan(arr[i])) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static boolean isPrimeNumber(int n) {
+        // so nguyen n < 2 khong phai la so nguyen to
+        if (n < 2) {
+            return false;
+        }
+        // check so nguyen to khi n >= 2
+        int squareRoot = (int) Math.sqrt(n);
+        for (int i = 2; i <= squareRoot; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static int totalprimeNumber(int arr[], int n) {
+        int total = 0;
+        for (int i = 0; i < n; i++) {
+            if (isPrimeNumber(arr[i])) {
+                total += arr[i];
+            }
+        }
+        return total;
+    }
 }
--- LapTrinhJava/buoi9/src/Employee.java
+++ LapTrinhJava/buoi9/src/Employee.java
@@ -1,2 +1,108 @@
-package PACKAGE_NAME;public class Employee {
+import java.util.Scanner;
+
+public class Employee {
+
+    public static void main(String[] args) {
+        int n;
+        Scanner scanner = new Scanner(System.in);
+        n = nhapSoNguyen(scanner);
+
+        String listName[] = new String[n];
+        int listAge[] = new int[n];
+        String listGender[] = new String[n];
+        int listSalary[] = new int[n];
+        float listMediumScore[] = new float[n];
+
+        inputInformationListEmployee(scanner, listName, listAge, listGender, listSalary, listMediumScore, n);
+        outputInformationListEmployee(listName, listAge, listGender, listSalary, listMediumScore, n);
+    }
+
+    static int nhapSoNguyen(Scanner scanner) {
+        int n;
+        do {
+            System.out.print("Nhập vào n: ");
+            n = scanner.nextInt();
+            if (n < 0) {
+                System.out.println("ban da nhap sai, vui long nhap lai!");
+            }
+        } while (n < 0);
+
+        return n;
+    }
+//    static void inputInformationEmployee(Scanner scanner, String name, int age, String gender, int salary, float mediumScore) {
+////        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Nhap vao tên nhân viên: ");
+//        name = scanner.nextLine();
+//        System.out.print("Nhap vào năm sinh: ");
+//        age = scanner.nextInt(); scanner.nextLine();
+//        System.out.print("Nhap vào giới tính: ");
+//        gender = scanner.nextLine();
+//        System.out.print("Nhập lương: ");
+//        salary = scanner.nextInt();
+//        System.out.print("Nhập điểm trung bình đại học: ");
+//        mediumScore = scanner.nextFloat(); scanner.nextLine();
+//    }
+
+    static String inputName(Scanner scanner) {
+        if (scanner.hasNextLine()) {
+            scanner.nextLine();
+        }
+        System.out.print("Nhap vao tên nhân viên: ");
+        return scanner.nextLine();
+    }
+
+    static String inputGender(Scanner scanner) {
+        if (scanner.hasNextLine()) {
+            scanner.nextLine();
+        }
+        System.out.print("Nhap vào giới tính: ");
+        return scanner.nextLine();
+    }
+
+    static int inputAge(Scanner scanner) {
+        System.out.print("Nhap vào năm sinh: ");
+        return scanner.nextInt();
+    }
+
+    static int inputSalary(Scanner scanner) {
+        System.out.print("Nhập lương: ");
+        return scanner.nextInt();
+    }
+
+    static float inputMediumScore(Scanner scanner) {
+        System.out.print("Nhập điểm trung bình đại học: ");
+        return scanner.nextFloat();
+    }
+
+    static void inputInformationListEmployee(Scanner scanner, String listName[], int listAge[], String listGender[], int listSalary[], float listMediumScore[], int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println("Nhập vào nhân viên thứ " + (i + 1) + " :");
+//            inputInformationEmployee(scanner, listName[i], listAge[i], listGender[i], listSalary[i], listMediumScore[i]);
+            listName[i] = inputName(scanner);
+            listAge[i] = inputAge(scanner);
+            listGender[i] = inputGender(scanner);
+            listSalary[i] = inputSalary(scanner);
+            listMediumScore[i] = inputMediumScore(scanner);
+        }
+    }
+
+    static void outputInformationEmployee(String name, int age, String gender, int salary, float mediumScore) {
+        System.out.println("Tên nhân viên: " + name);
+        System.out.println("Năm sinh: " + age);
+        System.out.println("Giới tính: " + gender);
+        System.out.println("Lương nhân viên: " + salary);
+        System.out.println("Điểm tb đại học: " + mediumScore);
+    }
+
+    static void outputInformationListEmployee(String listName[], int listAge[], String listGender[], int listSalary[], float listMediumScore[], int n) {
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Xuat nhân viên thứ " + (i + 1) + ": ");
+            outputInformationEmployee(listName[i], listAge[i], listGender[i], listSalary[i], listMediumScore[i]);
+        }
+    }
+
+    static void sortEmployee() {
+
+    }
 }
